ec3ef72c6aa96c19857002391bb84d310d71e1b2
AmyHolmess <569375355@qq.com>
2018-04-15 11:55:26 +0800

    注释

--- monte_carlo_tree_search/Situation.cpp
+++ monte_carlo_tree_search/Situation.cpp
@@ -25,26 +25,35 @@ vector<Piece>* Situation::get_situation(const int* board) {
 }
 int Situation::judge(const int* board)
 {
-	currentSituation = get_situation(board);
+	currentSituation = get_situation(board);//获得当前局面
 	vector<Piece>* hold;
-	int si = currentSituation->size();
+	int si = currentSituation->size();//当前局面的棋子数
 	for (int i = 0; i < totalSituation[si]->size(); ++i)
 	{
+		//在总局面已记录的棋子数为si的所有局面中查找当前局面的匹配
 		hold = (*totalSituation[si]).at(i);
-		for (int k = 0; k < 3; ++k) {
+		for (int k = 0; k < 3; ++k) {//四个方向遍历
 			for (int j = 0; j < hold->size(); ++j)
 				if (currentSituation->at(j) != hold->at(i)) {
 					*currentSituation = rotate(*currentSituation);
 					break;
 				}
-			return 1;
+			return 1;//匹配成功返回对应值
 		}
 	}
+	//匹配不成功，将当前局面添加到总局面，返回0
 	add_situation(currentSituation);
 	return 0;
 }
+bool Situation::SortPiece(const Piece& p1, const Piece& p2)
+{
+	if (p1.x != p2.x)
+		return p1.x < p2.x;
+	else return p1.y < p2.y;
+}
 vector<Piece>& Situation::rotate(vector<Piece>& sit)
 {
+	//将局面sit顺时针旋转90度，并排列
 	vector<Piece> s(sit.size());
 	Piece hold1, hold2;
 	for (int i = 0; i < sit.size(); i++) {
@@ -52,7 +61,7 @@ vector<Piece>& Situation::rotate(vector<Piece>& sit)
 		hold1.Set(hold2.y, SIZE - 1 - hold2.x, hold2.c);
 		s.push_back(hold1);
 	}
-	sort(s.begin(), s.end(), Sortx);
+	sort(s.begin(), s.end(), SortPiece);
 	return s;
 }
 bool Situation::add_situation(vector<Piece>* s)
--- monte_carlo_tree_search/Situation.h
+++ monte_carlo_tree_search/Situation.h
@@ -20,7 +20,7 @@ class Piece
 		else return true;
 	}
 public:
-	int x; int y; int c;
+	int x; int y; int c;//x，y为棋子在棋盘上的横纵坐标，c为棋子颜色
 	Piece(int sx = 0, int sy = 0, int sc = 0) { x = sx; y = sy; c = sc; }
 	Piece(const Action& a) { x = a.get_x() - 1; y = a.get_y() - 1; c = 0; }
 	void Set(int sx, int sy, int sc) { x = sx; y = sy; c = sc; }
@@ -33,15 +33,20 @@ public:
 	Situation();
 	~Situation();
 	
+	//获得当前棋盘对应的局面
 	vector<Piece>* get_situation(const int* board);
+	//判断当前局面是否在totalSituation中有匹配
 	int judge(const int* board);
-
+	
+	//将局面s添加到总局面中
 	bool add_situation(vector<Piece>* s);
 
-	bool Sortx(const Piece& p1, const Piece& p2) { return p1.x < p2.x; }
+	bool SortPiece(const Piece& p1, const Piece& p2);//sort函数排序判断
+	//将局面sit旋转90度
 	vector<Piece>& rotate(vector<Piece>& sit);
 private:
-	vector<vector<Piece>*>* totalSituation[100];
-	vector<Piece>* currentSituation;
+	//局面为棋盘上所有落子位置的记录向量，按i的升序排列，局面的size是棋盘上的落子数
+	vector<vector<Piece>*>* totalSituation[100];//所有的局面，totalSituation[i]为指向棋子数为i的局面的总和的向量指针
+	vector<Piece>* currentSituation;//当前局面
 };
 #endif // !SITUATION_H
